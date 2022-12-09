9b4ac71b614461c7bfc56a081601770b594ad58e
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-12-08 19:58:18 -0800

    Implement day 1 part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,23 +1,98 @@
-fn main() {
-    // Set up the source data...
-    let srcData = vec![
-        Some(1000),
-        Some(2000),
-        Some(3000),
-        None,
-        Some(4000),
-        None,
-        Some(5000),
-        Some(6000),
-        None,
-        Some(7000),
-        Some(8000),
-        Some(9000),
-        None,
-        Some(10000),
-    ];
+use std::fs;
 
+
+const INPUT_FILE: &str = "inputs/day-01.txt";
+
+
+fn read_input_from_file(path: &str) -> Result<Vec<Option<i32>>, String> {
+    // Read in the file...
+    let str_or_err = fs::read_to_string(path);
     
+    // Check for an error...
+    if let Err(err) = str_or_err {
+        return Err(format!("failed to read in file at \"{}\": {}", path, err));
+    }
 
-    println!("Hello, world!");
+    // Otherwise, must be a valid string (this should never panic)...
+    let raw_str = str_or_err.unwrap();
+
+    // Iterate through the lines...
+    let mut res: Vec<_> = raw_str
+        .trim()
+        .split("\n")
+        .map(|s| {
+            // Trim any unwanted space on that line...
+            let s = s.trim();
+
+            // If the line is empty, return None
+            if s == "" {
+                return None
+            }
+
+            // Otherwise, attempty to parse it as an int...
+            let n = s
+                .to_string()
+                .parse::<i32>()
+                .unwrap();
+
+            // Return it!
+            Some(n)
+
+        })
+        .collect()
+        ;
+
+    // Push another "None" at the end of the line...
+    res.push(None);
+
+    // Return success!
+    Ok(res)
+}
+
+
+fn do_work(data: Vec<Option<i32>>) -> Option<i32> {
+    // Create a variable to store the result...
+    let mut max = None;
+    let mut cur = 0;
+
+    // Iterate through the data...
+    for n in data.iter() {
+        match n {
+            // If n is none (aka line is empty) running tally is complete.
+            // Check against the max.
+            None => {
+                // Is the running amount bigger that the previous max?
+                // Or is the previous max none?
+                match max {
+                    Some(maxn) => { // There is a previous max...
+                        if cur > maxn { // ...and current is larger
+                            max = Some(cur);
+                        }
+                    },
+                    None => {
+                        max = Some(cur);
+                    },
+                }
+                
+                // Reset the running tally
+                cur = 0;
+            },
+
+            // Otherwise, add it to the tally...
+            Some(n) => {
+                cur += n;
+            },
+        }
+    }
+    
+    // Return the result...
+    max
+}
+
+fn main() {
+    let input_data = read_input_from_file(INPUT_FILE);
+    let input_data = input_data.expect("Failed to read input data file");
+
+    let res = do_work(input_data);
+    println!("Result: {:?}", res);
 }

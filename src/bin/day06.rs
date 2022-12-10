9347c28c2c61373d5fb1991d7161771edf559566
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-12-10 10:56:42 -0800

    Implement day 6 part 1

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,7 +1,7 @@
-// use std::fs;
+use std::fs;
 use std::collections::HashSet;
 
-// const INPUT_FILE: &str = "inputs/day-06.txt";
+const INPUT_FILE: &str = "inputs/day-06.txt";
 const BUF_SIZE: usize = 4;
 
 fn find(raw: &str) -> Option<usize> {
@@ -16,15 +16,8 @@ fn find(raw: &str) -> Option<usize> {
 }
 
 fn main() {
-    assert_eq!(find("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(7));
-    assert_eq!(find("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
-    assert_eq!(find("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
-    assert_eq!(find("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
-    assert_eq!(find("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
-
-
-    let raw = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-    if let Some(i) = find(raw) {
+    let raw = fs::read_to_string(INPUT_FILE).unwrap();
+    if let Some(i) = find(raw.as_str()) {
         println!("Found a match at index {}", i);
     } else {
         println!("No match found!");

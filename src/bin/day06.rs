06e1cd8e17897305ea0802719a6299dd33103ba2
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-12-10 10:59:02 -0800

    Implement day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -2,14 +2,14 @@ use std::fs;
 use std::collections::HashSet;
 
 const INPUT_FILE: &str = "inputs/day-06.txt";
-const BUF_SIZE: usize = 4;
+const BUF_SIZE: usize = 14; // 4;
 
-fn find(raw: &str) -> Option<usize> {
-    for i in 0..(raw.len()-BUF_SIZE) {
-        let chunk = &raw[i..i+BUF_SIZE];
+fn find(raw: &str, size: usize) -> Option<usize> {
+    for i in 0..(raw.len()-size) {
+        let chunk = &raw[i..i+size];
         let chars: HashSet<_> = chunk.chars().collect();
-        if chars.len() == BUF_SIZE {
-            return Some(i + BUF_SIZE);
+        if chars.len() == size {
+            return Some(i + size);
         }
     }
     None
@@ -17,7 +17,7 @@ fn find(raw: &str) -> Option<usize> {
 
 fn main() {
     let raw = fs::read_to_string(INPUT_FILE).unwrap();
-    if let Some(i) = find(raw.as_str()) {
+    if let Some(i) = find(raw.as_str(), BUF_SIZE) {
         println!("Found a match at index {}", i);
     } else {
         println!("No match found!");

3496d9d8ce4ab025becd98651be800170b88e231
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-12-12 13:16:48 -0800

    Working on day 9 part 1

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,15 +1,78 @@
 use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Move {
-    Right(i32),
-    Left(i32),
-    Down(i32),
-    Up(i32),
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Move (Direction, i32);
+
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Pos(i32, i32);
+
+
+fn move_head(p: Pos, d: Direction) -> Pos {
+    // Extract the x and y coordinates from the head's position
+    let Pos(x, y) = p;
+
+    // Move accordingly...
+    match d {
+        Direction::Up => Pos(x, y-1),
+        Direction::Down => Pos(x, y+1),
+        Direction::Left => Pos(x-1, y),
+        Direction::Right => Pos(x+1, y),
+    }
+}
+
+fn move_tail(head: Pos, tail: Pos) -> Pos {
+    // Extract the x and y coords for head and tail...
+    let Pos(hx, hy) = head;
+    let Pos(tx, ty) = tail;
+
+    // Calculate the distances / deltas...
+    let dx = hx - tx;
+    let dy = hy - ty;
+
+    // Calculate the absolute values of the distances...
+    let adx = dx.abs();
+    let ady = dy.abs();
+
+    // If the tail's distance is w/in one square (including diagonals), do nothing...
+    if adx <= 1 && ady <= 1 {
+        return Pos(tx, ty);
+    }
+
+    // Otherwise, move it one space in one direction based on
+    // which direction has the greater difference.
+
+    // Is the x-distance greater?
+    if adx > ady && adx > 0 { // Is the x-diff positive? ...then move up one x
+        return Pos(tx+1, ty);
+
+    } else if adx > ady && adx < 0 { // Is the x-diff negative? ...then move down one x
+        return Pos(tx-1, ty);
+
+    }
+    
+    // Otherwise, is the y-distance greater?
+    if ady > adx && ady > 0 { // Is the y-diff positive? ...then move up one y
+        return Pos(tx, ty+1);
+
+    } else if ady > adx && ady < 0 { // Is the y-diff negative? ...then move down one y
+        return Pos(tx, ty-1);
+    }
+
+    // Note: I'm ASSUMING that the above two if-statements are
+    // exhaustive but I'm leaving this panic in, just in case
+    // I'm wrong...
+    unreachable!("weird difference for head={:?} and tail={:?}", head, tail);
+}
 
 
 fn main() {
@@ -39,10 +102,10 @@ R 2";
 
             // Return the move enum based on letter...
             match letter {
-                "U" => Move::Up(count),
-                "D" => Move::Down(count),
-                "L" => Move::Left(count),
-                "R" => Move::Right(count),
+                "U" => Move(Direction::Up, count),
+                "D" => Move(Direction::Down, count),
+                "L" => Move(Direction::Left, count),
+                "R" => Move(Direction::Right, count),
                 _ => unreachable!(),
             }
         })
@@ -53,14 +116,25 @@ R 2";
         let mut head = Pos(0, 0);
         let mut tail = Pos(0, 0);
         let mut visited: HashSet<Pos> = HashSet::new();
+        
+        // Add the tail's starting position...
+        visited.insert(tail);
 
         // Iterate through the moves...
         for m in moves {
-            match m {
-                Move::Up(n) => {},
-                Move::Down(n) => {},
-                Move::Left(n) => {},
-                Move::Right(n) => {},
+            // Split the move into direction and number of moves...
+            let Move(d, n) = m;
+
+            // Iterate through the count, applying each move...
+            for _ in 0..n {
+                // Move the head...
+                head = move_head(head, d);
+
+                // Move the tail...
+                tail = move_tail(head, tail);
+
+                // Add the tail's position to the visited set...
+                visited.insert(tail);
             }
         }
 

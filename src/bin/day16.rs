fb1f3117be9260077449d99a3933b478d40af2a5
a-poor <45295232+a-poor@users.noreply.github.com>
2023-01-02 15:18:52 -0800

    Rewrite d16p1 myself

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -5,56 +5,60 @@ use std::fs;
 use std::collections::{HashMap, VecDeque};
 use regex::Regex;
 
+
+#[derive(Debug, Clone)]
 struct Valve {
-    flow_rate: i32,
+    name: String,
+    flow_rate: u32,
     tunnels: Vec<String>,
 }
 
-fn dfs(valves: &HashMap<String, Valve>, max_time: i32, elapsed_time: i32, pressure: i32, visited: &mut Vec<String>) -> i32 {
-    // Return the current pressure if we have exceeded the maximum time
-    if elapsed_time > max_time {
-        return pressure;
-    }
+fn parse_line(line: &str) -> Result<Valve, String> {
+    // Form the regex...
+    let re = Regex::new(r"Valve ([A-Z]{2}) has flow rate=([0-9]+); tunnels? leads? to valves? (.+)")
+        .expect("failed to compile regex");
 
-    // Keep track of the maximum pressure achieved
-    let mut max_pressure = pressure;
+    // Parse the line...
+    let caps = re.captures(line)
+        .ok_or(format!("failed to parse line: {}", line))?;
 
-    // Explore each unvisited tunnel from the current valve
-    for tunnel in &valves[&visited[visited.len() - 1]].tunnels {
-        if !visited.contains(tunnel) {
-            // Open the valve and add it to the visited list
-            visited.push(tunnel.to_string());
-            let new_pressure = pressure + valves[tunnel].flow_rate;
+    // Extract the parts...
+    let name = caps
+        .get(1)
+        .unwrap()
+        .as_str()
+        .to_string();
+    let flow_rate = caps
+        .get(2)
+        .unwrap()
+        .as_str()
+        .parse::<u32>()
+        .map_err(|e| format!("failed to parse flow rate: {}", e))?;
+    let tunnels = caps
+        .get(3)
+        .unwrap()
+        .as_str()
+        .split(", ")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
 
-            // Recursively search from the new valve
-            let p = dfs(valves, max_time, elapsed_time + 2, new_pressure, visited);
-            max_pressure = i32::max(max_pressure, p);
-
-            // Backtrack by closing the valve and removing it from the visited list
-            visited.pop();
-        }
-    }
-
-    max_pressure
+    // Success!
+    Ok(Valve { name, flow_rate, tunnels })
 }
 
-fn main() {
-    // Read the input file and parse the valves and tunnels
-    let input = fs::read_to_string("inputs/day-16.txt").unwrap();
-    let valve_regex = Regex::new(r"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)").unwrap();
-    let mut valves = HashMap::new();
-    for line in input.lines() {
-        let caps = valve_regex.captures(line).unwrap();
-        let name = caps[1].to_string();
-        let flow_rate = caps[2].parse::<i32>().unwrap();
-        let tunnels: Vec<String> = caps[3].split(", ").map(|s| s.to_string()).collect();
-        valves.insert(name, Valve { flow_rate, tunnels });
-    }
 
-    // Start the search from valve AA
-    let mut visited = vec!["AA".to_string()];
-    let max_pressure = dfs(&valves, 30, 0, 0, &mut visited);
-
-    println!("Maximum pressure release: {}", max_pressure);
+struct State {
+    valves: HashMap<String, Valve>,
+    queue: VecDeque<String>,
+    visited: HashMap<String, u32>,
 }
 
+fn main() -> Result<(), String> {
+    // Read the input data...
+    let input = aoc_22::util::load_input(16, true)?;
+
+
+
+    // Success!
+    Ok(())
+}

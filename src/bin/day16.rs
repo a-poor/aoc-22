16eedb47767caf55e06b62bb0b0216d2b25d1f83
a-poor <45295232+a-poor@users.noreply.github.com>
2023-01-02 15:09:44 -0800

    Use ChatGPT's d16p1 solution

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,211 +1,60 @@
-/// -- Notes ------------
-/// * Release as much pressure as possible in the allotted time 
-/// * 30m total
-/// * 1m to open a valve  
-/// * 1m to go through a tunnel
-/// * Can only open a valve once...
+#![allow(unused_imports)]
+#![allow(dead_code)]
 
-#[allow(dead_code)]
-use std::collections::{HashMap, HashSet};
-use rand::Rng;
-use rand::rngs::ThreadRng;
+use std::fs;
+use std::collections::{HashMap, VecDeque};
+use regex::Regex;
 
-const START_TIME: i32 = 30;
-
-
-#[allow(dead_code)]
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Valve {
-    name: String,
     flow_rate: i32,
     tunnels: Vec<String>,
 }
 
-#[allow(dead_code)]
-impl Valve {
-    fn new(name: String, flow_rate: i32, tunnels: Vec<String>) -> Valve {
-        Valve { name, flow_rate, tunnels }
+fn dfs(valves: &HashMap<String, Valve>, max_time: i32, elapsed_time: i32, pressure: i32, visited: &mut Vec<String>) -> i32 {
+    // Return the current pressure if we have exceeded the maximum time
+    if elapsed_time > max_time {
+        return pressure;
     }
 
-    fn parse_line(line: &str) -> Valve {
-        let re = regex::Regex::new(r"Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z]+(, [A-Z]+)*)").unwrap();
-        let caps = re
-            .captures(line)
-            .expect("no captures found");
-        let name = caps
-            .get(1)
-            .expect("couldnt' find valve name match")
-            .as_str()
-            .to_string();
-        let flow_rate = caps
-            .get(2)
-            .expect("couldn't get flow rate match")
-            .as_str()
-            .parse::<i32>()
-            .expect("couldn't parse flow rate as int");
-        let tunnels = caps
-            .get(3)
-            .expect("couldn't get tunnels match")
-            .as_str()
-            .split(", ")
-            .map(|s| s.to_string())
-            .collect();
-        Valve::new(name, flow_rate, tunnels)
-    }
-}
+    // Keep track of the maximum pressure achieved
+    let mut max_pressure = pressure;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-enum Move {
-    OpenValve,
-    GoThroughTunnel(String),
-}
+    // Explore each unvisited tunnel from the current valve
+    for tunnel in &valves[&visited[visited.len() - 1]].tunnels {
+        if !visited.contains(tunnel) {
+            // Open the valve and add it to the visited list
+            visited.push(tunnel.to_string());
+            let new_pressure = pressure + valves[tunnel].flow_rate;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct GameState {
-    time: i32,
-    pressure: i32,
-    opened: HashSet<String>,
-}
+            // Recursively search from the new valve
+            let p = dfs(valves, max_time, elapsed_time + 2, new_pressure, visited);
+            max_pressure = i32::max(max_pressure, p);
 
-#[allow(dead_code)]
-impl GameState {
-    fn new() -> GameState {
-        GameState { pressure: 0, time: START_TIME, opened: HashSet::new() }
-    }
-
-    fn is_over(&self) -> bool {
-        self.time <= 0
-    }
-
-    fn is_open(&self, valve: &Valve) -> bool {
-        self.opened.contains(&valve.name)
-    }
-
-    fn tick(&mut self) {
-        self.time -= 1;
-    }
-
-    fn open_valve(&mut self, valve: &Valve) -> i32 {
-        if self.is_open(valve) {
-            return 0;
-        }
-        
-        self.opened.insert(valve.name.clone());
-        self.pressure += valve.flow_rate;
-        valve.flow_rate
-    }
-}
-
-struct GameSetup {
-    /// How to update the existing 
-    learning_rate: f32,
-
-    /// How likely we are to explore new options 
-    /// vs exploit what we already know
-    epsilon: f32,
-}
-
-struct GameResult {
-    pressure_released: i32,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-struct StateMamanger {
-    valves: Vec<Valve>,
-    memory: HashMap<String, HashMap<Move, f32>>,
-}
-
-#[allow(dead_code)]
-impl StateMamanger {
-    fn new(valves: Vec<Valve>) -> StateMamanger {
-        // Initialize the empty memory...
-        let mut memory = HashMap::new();
-
-        // For each valve...
-        for valve in &valves {
-            // Create a map of moves to their Q values...
-            let mut moves = HashMap::new();
-            
-            // Add the option to open the valve...
-            moves.insert(Move::OpenValve, 0.0);
-
-            // Add the option to go through each tunnel...
-            for tunnel in &valve.tunnels {
-                moves.insert(Move::GoThroughTunnel(tunnel.clone()), 0.0);
-            }
-
-            // Add the map to the memory...
-            memory.insert(valve.name.clone(), moves);
-        }
-
-        // Initialize and return...
-        StateMamanger { 
-            valves,
-            memory,
+            // Backtrack by closing the valve and removing it from the visited list
+            visited.pop();
         }
     }
 
-    fn run_a_game(&mut self, setup: GameSetup) -> GameResult {
-        // Create the random number generator...
-        let mut rng = rand::thread_rng();
-
-        // Create the game state...
-        let mut state = GameState::new();
-
-        // While the game is not over...
-        while !state.is_over() {
-
-            // For each valve...
-            for valve in &self.valves {
-
-                // If the valve is not open...
-                if state.is_open(valve) {
-
-                    // Get the Q values for the valve...
-                    let q_values = self.memory.get(&valve.name).unwrap();
-
-                    // Get the best move...
-                    let best_move = self.get_best_move(q_values, &mut rng, setup.epsilon);
-
-                    // Perform the move...
-                    match best_move {
-                        Move::OpenValve => {
-                            // Open the valve...
-                            self.opened.insert(valve.name.clone());
-
-                            // Update the game state...
-                            state.dec_time();
-                            state.inc_pressure(valve.flow_rate);
-                        },
-                        Move::GoThroughTunnel(tunnel) => {
-                            // Update the game state...
-                            state.dec_time();
-                        },
-                    }
-                }
-            }
-        }
-
-        // Return the result...
-        GameResult { pressure_released: state.pressure }
-    }
-
-    fn get_best_move(&self) -> Move {
-        Move::OpenValve
-    }
+    max_pressure
 }
 
-fn main() -> Result<(), String> {
-    let raw = aoc_22::util::load_input(16, true)?;
-    let valves = raw.split("\n").map(|line| Valve::parse_line(line)).collect();
-    let state = StateMamanger::new(valves);
-
-    for valve in &state.valves {
-        println!("{:?}", valve);
+fn main() {
+    // Read the input file and parse the valves and tunnels
+    let input = fs::read_to_string("inputs/day-16.txt").unwrap();
+    let valve_regex = Regex::new(r"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)").unwrap();
+    let mut valves = HashMap::new();
+    for line in input.lines() {
+        let caps = valve_regex.captures(line).unwrap();
+        let name = caps[1].to_string();
+        let flow_rate = caps[2].parse::<i32>().unwrap();
+        let tunnels: Vec<String> = caps[3].split(", ").map(|s| s.to_string()).collect();
+        valves.insert(name, Valve { flow_rate, tunnels });
     }
 
-    Ok(())
+    // Start the search from valve AA
+    let mut visited = vec!["AA".to_string()];
+    let max_pressure = dfs(&valves, 30, 0, 0, &mut visited);
+
+    println!("Maximum pressure release: {}", max_pressure);
 }
+
